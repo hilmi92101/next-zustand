77ad5e7831c53f785a1c1e65e1b6459e55d8f968
hilmi <muhammadhilmi92101@gmail.com>
2024-07-01 16:17:24 +0800

    setup shadcn

---
+++ src/app/test-shadcn/page.tsx
@@ -0,0 +1,18 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+
+const testShadcnPage = () => {
+    return (
+        <>
+            <div className="py-8">
+                <h1 className="text-5xl">Buttons</h1>
+            </div>
+
+            <div className="py-4">
+                <Button>Click me</Button>
+            </div>
+
+        </>
+    )
+}
+
+export default testShadcnPage
